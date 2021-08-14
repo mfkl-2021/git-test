cda811517fccae5d888bce8fc111301b732c3c44
mfkl-2021 <77180041+mfkl-2021@users.noreply.github.com>
2021-08-14 23:06:17 +0800

    pull commit

--- src/main/java/com/mfkl/git/GitTest.java
+++ src/main/java/com/mfkl/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("Hello Git! master test ");
         System.out.println("Hello Git!2");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
